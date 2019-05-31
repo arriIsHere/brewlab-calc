58355be9879e7d881a1fb3e41a1bc4b8f89bd6b9
bblais23 <ben.blais6@gmail.com>
2019-05-31 18:46:57 -0400

    add documentation for infusion temperature.

--- src/infusionTemperature.js
+++ src/infusionTemperature.js
@@ -1,7 +1,21 @@
+/**
+ * Provides the temperature of the water to be added to a mash to achieve an 
+ * initial infusion temperature. This will not do calculations for subsequent
+ * infusions.
+ * @param {number} waterGrainRatio The ratio of water to grains in qts to lbs.
+ * for example 1.5 on a batch of 10lb of grain means that you intend to add 
+ * 15 qts (3.75 gallons) of water to the mash with the grains.
+ * @param {number} targetTemperature Intended temperature (in degrees 
+ * fahrenheit) of mash after initial infusion.
+ * infusion.
+ * @param {number} grainTemperature The temperature of the grains (in degrees 
+ * fahrenheit) that will be added to the mash. If none are supplied it will
+ * default to room temperature (72 F).
+ */
 export default function infusionTemperature(
     waterGrainRatio, 
     targetTemperature, 
-    grainTemperature,
+    grainTemperature = 72,
 ) {
     const constantOverRatio = 0.2 / waterGrainRatio;
     const temperatureDelta = targetTemperature - grainTemperature;
