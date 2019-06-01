a4be9d393a2d431ff517903da5052f3cb1e26d00
bblais23 <ben.blais6@gmail.com>
2019-06-01 00:14:57 -0400

    test for original gravity

---
+++ src/originalGravity.test.js
@@ -0,0 +1,50 @@
+import {originalGravity} from './index';
+
+describe('original gravity', ()=> {
+    it('standard baseline', () => {
+        const gravityPoints = 360;
+        const volume = 5;
+        const mashEfficiency = 80;
+
+        const ogExpected = 1.058;
+
+        const og = originalGravity(gravityPoints, volume, mashEfficiency);
+
+        expect(og.toFixed(3)).toBe(ogExpected.toFixed(3));
+    });
+
+    it('default efficiency', () => {
+        const gravityPoints = 360;
+        const volume = 5;
+
+        const ogExpected = 1.058;
+
+        const og = originalGravity(gravityPoints, volume);
+
+        expect(og.toFixed(3)).toBe(ogExpected.toFixed(3));
+    });
+
+    it('High gravity', () => {
+        const gravityPoints = 720;
+        const volume = 5;
+        const mashEfficiency = 80;
+
+        const ogExpected = 1.115;
+
+        const og = originalGravity(gravityPoints, volume, mashEfficiency);
+
+        expect(og.toFixed(3)).toBe(ogExpected.toFixed(3));
+    });
+
+    it('Large Volume', () => {
+        const gravityPoints = 720;
+        const volume = 10;
+        const mashEfficiency = 80;
+
+        const ogExpected = 1.058;
+
+        const og = originalGravity(gravityPoints, volume, mashEfficiency);
+
+        expect(og.toFixed(3)).toBe(ogExpected.toFixed(3));
+    });
+});
