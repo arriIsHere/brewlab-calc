fe833ad637fbae850a097ba848d3f1c5828cea9d
bblais23 <ben.blais6@gmail.com>
2019-05-31 23:57:58 -0400

    document original gravity calculator.

--- src/originalGravity.js
+++ src/originalGravity.js
@@ -1,4 +1,14 @@
-export default function originalGravity(gravityPoints, volume, mashEfficiency) {
+/**
+ * Calculates the original gravity (e.x. 1.050) given the gravity points, 
+ * volume, and efficiency.
+ * @param {number} gravityPoints The total number of gravity points from all
+ * the grains, extract, and adjuncts. Please note that this is gravity points
+ * and not a gravity value. For example 1.050 would not be correct but 50 would be.
+ * @param {number} volume Volume of the batch in gallons.
+ * @param {number} mashEfficiency Percentage efficiency of the total brewhouse efficiency.
+ * For extract brewing use 100.
+ */
+export default function originalGravity(gravityPoints, volume, mashEfficiency = 80) {
     const efficiencyFraction = mashEfficiency / 100;
     const brewhouseGravityPoints = gravityPoints * efficiencyFraction; 
     const volumeGravityPoints = brewhouseGravityPoints / volume;
