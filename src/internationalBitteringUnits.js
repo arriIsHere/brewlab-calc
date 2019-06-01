15847c17c463c54fcc7a0c097d6bf2f17da5d176
bblais23 <ben.blais6@gmail.com>
2019-05-31 23:23:13 -0400

    document and update ibu calc

--- src/internationalBitteringUnits.js
+++ src/internationalBitteringUnits.js
@@ -13,8 +13,24 @@ const utilizationTable = [
     31,
 ];
 
-
-export default function internationalBitteringUnits(alphaAcids, boilTime, weight) {
+/**
+ * Estimates the bitterness of the beer based on a hop addition as 
+ * International Bittering Units. This is a estimate of bitterness not an 
+ * actual measure and the hop addition may yield more or less bitterness
+ * than this calculation shows.
+ * @param {number} alphaAcids alpha acids as a percentage for this hop addition
+ * @param {number} boilTime time in minutes that this hop will be in the boil.
+ * for example, a value of 60 means the hops will be in for the entirety of a
+ * 60minute boil.
+ * @param {number} weight weight in ounces
+ * @param {number} volume volume of boil in gallons
+ */
+export default function internationalBitteringUnits(
+    alphaAcids, 
+    boilTime, 
+    weight, 
+    volume,
+) {
     let utilization = utilizationTable[13];
     if(boilTime <= 60) {
         utilization = utilizationTable[Math.floor(boilTime / 5)]
