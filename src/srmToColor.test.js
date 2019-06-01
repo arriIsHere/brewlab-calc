8f7d0c0f93c406b8cb41429d0e46022bbdbbe9c4
bblais23 <ben.blais6@gmail.com>
2019-06-01 00:19:36 -0400

    update documentation for srm to color function.

---
+++ src/srmToColor.test.js
@@ -0,0 +1,9 @@
+import { srmToColor } from './index';
+
+describe('SRM to Color', () => {
+    it('return check', () => {
+        const srm = 5;
+
+        const color = srmToColor(srm);
+    });
+});
