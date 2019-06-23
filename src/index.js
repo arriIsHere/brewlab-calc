f36b87807c22a6553d5e9622ff9eecd47814262d
bblais23 <ben.blais6@gmail.com>
2019-06-23 11:53:37 -0400

    add og -> brix

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ export { default as finalGravity } from './finalGravity';
 export { default as infusionTemperature } from './infusionTemperature';
 export { default as internationalBitteringUnits } from './internationalBitteringUnits';
 export { default as originalGravity } from './originalGravity';
+export { default as originalGravityToBrix } from './originalGravityToBrix';
 export { default as srmToColor } from './srmToColor';
