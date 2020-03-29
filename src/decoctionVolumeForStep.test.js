413621160f3a5cd27a90539bdff30b8655a399c9
Benjamin Blais <ben.blais6@gmail.com>
2020-03-29 09:57:55 -0400

    results as two decimal places

--- src/decoctionVolumeForStep.test.js
+++ src/decoctionVolumeForStep.test.js
@@ -2,10 +2,10 @@ import decoctionVolumeForStep from './decoctionVolumeForStep';
 
 describe('Decoction Volume for Step unit test', () => {
 	it.each([
-		[70, 105, 5.65, 1.4],
-		[105, 122, 5.65, 0.85],
+		[70, 105, 5.65, 1.39],
+		[105, 122, 5.65, 0.90],
 		[122, 150, 5.65, 1.76],
-		[150, 170, 5.65, 1.8],
+		[150, 170, 5.65, 1.82],
 	])('(%s, %s) => %s', (startTemperature, targetTemperature, mashVolume, expectedVolume) => {
 		const measuredTemperature = decoctionVolumeForStep(startTemperature, targetTemperature, mashVolume);
 
