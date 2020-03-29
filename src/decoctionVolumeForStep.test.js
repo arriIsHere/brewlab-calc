ba955dc1a6df387eb965b780f72a5e6052d4aa8f
Benjamin Blais <ben.blais6@gmail.com>
2020-03-29 09:55:02 -0400

    take mash volume into account

--- src/decoctionVolumeForStep.test.js
+++ src/decoctionVolumeForStep.test.js
@@ -2,12 +2,12 @@ import decoctionVolumeForStep from './decoctionVolumeForStep';
 
 describe('Decoction Volume for Step unit test', () => {
 	it.each([
-		[70, 105, 1.4],
-		[105, 122, 0.85],
-		[122, 150, 1.76],
-		[150, 170, 1.8],
-	])('(%s, %s) => %s', (startTemperature, targetTemperature, expectedVolume) => {
-		const measuredTemperature = decoctionVolumeForStep(startTemperature, targetTemperature);
+		[70, 105, 5.65, 1.4],
+		[105, 122, 5.65, 0.85],
+		[122, 150, 5.65, 1.76],
+		[150, 170, 5.65, 1.8],
+	])('(%s, %s) => %s', (startTemperature, targetTemperature, mashVolume, expectedVolume) => {
+		const measuredTemperature = decoctionVolumeForStep(startTemperature, targetTemperature, mashVolume);
 
 		expect(measuredTemperature).toBe(expectedVolume);
 	});
