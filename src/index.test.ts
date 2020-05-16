ede26abb935f604294dc4c830802d96f0c495e06
Benjamin Blais <ben.blais6@gmail.com>
2020-05-16 14:39:30 -0400

    fix ci tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,13 +2,15 @@ import fs from 'fs-extra';
 import path from 'path';
 import * as index from './index';
 
+const extension = process.env.NODE_ENV === 'ci' ? 'js' : 'ts';
+
 describe('Test for All imports', () => {
 	it('all named files imported in index', async () => {
 		const files = await fs.readdir(__dirname);
 
 		const functionFiles = files.filter(
-			file => !file.endsWith('.test.ts') && !file.includes('index.ts')
-		).map(file => path.basename(file, '.ts'));
+			file => !file.endsWith(`.test.${extension}`) && !file.includes(`index.${extension}`) && !file.endsWith('.d.ts')
+		).map(file => path.basename(file, `.${extension}`));
 
 		const indexRequireNames = Object.keys(index);
 
